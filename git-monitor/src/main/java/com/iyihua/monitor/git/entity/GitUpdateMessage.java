561ae6f0c825c88ad9e31bb0ce3c4ff1063956ce
yihuawanglv <wanglvyihua@gmail.com>
2017-04-28 18:17:48 +0800

    init

---
+++ git-monitor/src/main/java/com/iyihua/monitor/git/entity/GitUpdateMessage.java
@@ -0,0 +1,15 @@
+package com.iyihua.monitor.git.entity;
+
+public class GitUpdateMessage {
+
+	String key;
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+}
