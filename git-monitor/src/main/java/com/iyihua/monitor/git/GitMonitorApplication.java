cc333fd4c91bc2622cc7669a47c3803369fb2098
yihuawanglv <wanglvyihua@gmail.com>
2017-05-27 18:05:04 +0800

    更新

--- git-monitor/src/main/java/com/iyihua/monitor/git/GitMonitorApplication.java
+++ git-monitor/src/main/java/com/iyihua/monitor/git/GitMonitorApplication.java
@@ -2,6 +2,8 @@ package com.iyihua.monitor.git;
 
 import java.util.concurrent.TimeUnit;
 
+import com.iyihua.dispatcher.client.tcp.CuratorProvider;
+
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
@@ -27,6 +29,11 @@ public class GitMonitorApplication {
 		ObservableFuture<String> observableFuture = RxHelper.observableFuture();
 		Vertx.vertx().deployVerticle(Server.class.getName(), initOptionsConfig(), observableFuture.toHandler());
 		observableFuture.timeout(1, TimeUnit.MINUTES).toBlocking().first();
+		try {
+			new CuratorProvider("127.0.0.1:2181").register("git-monitor", "localhost:8300");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("**********************************");
 		System.out.println("Vertx server started successfully");
 		System.out.println("**********************************");
